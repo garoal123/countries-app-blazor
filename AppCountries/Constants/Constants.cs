f3ed31ef46e031e5d5dfa4db37a01d4c51f95008
juan.esquivel <juan.esquivel@banregio.com>
2024-09-24 14:54:41 -0600

    - Se genera vista para mostrar continente

---
+++ AppCountries/Constants/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AppCountries.Constants
+{
+	public class Constants
+	{
+		public Constants()
+		{
+		}
+
+        public static string[] Continents = { "Africa", "Americas", "Antarctic", "Asia", "Europe", "Oceania" };
+    }
+}
+
---
+++ AppCountries/Utils/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AppCountries.Utils
+{
+    public class Utils
+    {
+        public Utils()
+        {
+        }
+
+        // Función para traducir el nombre del continente de inglés a español
+        public static string TraducirContinente(string continenteIngles)
+        {
+            return continenteIngles switch
+            {
+                "Africa" => "África",
+                "Americas" => "América",
+                "Antarctic" => "Antártida",
+                "Asia" => "Asia",
+                "Europe" => "Europa",
+                "Oceania" => "Oceanía",
+                _ => "Admin" // O cualquier otro valor predeterminado
+            };
+        }
+
+        public static Dictionary<string, string> ContinentTranslations = new Dictionary<string, string>
+{
+        { "Africa", "África" },
+        { "Americas", "América" },
+        { "Antarctic", "Antártida" },
+        { "Asia", "Asia" },
+        { "Europe", "Europa" },
+        { "Oceania", "Oceanía" }
+    };
+    }
+}
